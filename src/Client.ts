fa5869b4814879df746fab8c55c3f30e3fad496b
evgenijegorov <zhekanes@yandex.ru>
2023-10-10 20:03:05 +0700

    with creds

--- src/Client.ts
+++ src/Client.ts
@@ -171,7 +171,7 @@ export class Client {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(options),
-                credentials: 'include'
+                withCredentials: true
             })
         ).data;
 
